6886a9f30fe51ec1c0341e187f9a53f8bb1c29cf
angela <angela@AngeladeMacBook-Air.local>
2020-11-12 21:17:54 +0800

    Add changes and comments to Chesspiece.cpp and Chesspiece.h

--- Chesspiece.cpp
+++ Chesspiece.cpp
@@ -1,23 +1,30 @@
 #include "Chesspiece.h"
-using namespace std;
 
+// Chesspiece
+// Purpose:     Initialize the pieceLabel as the blank space
+// Arguments:   None
+// Returns:     None
 Chesspiece::Chesspiece(){
     pieceLabel = ' ';
 }
 
-Chesspiece::Chesspiece(char piece){
-    setPieceLabel(piece);
-}
-
+// getPieceLabel
+// Purpose:     Get the pieceLabel of the Chesspiece type variable
+// Arguments:   None
+// Returns:     pieceLabel (char)
 const char Chesspiece::getPieceLabel()
 {
     return pieceLabel;
 }
 
-void Chesspiece::setPieceLabel(char Chesspiece){
-    if (Chesspiece == 'X' || Chesspiece == 'O' || Chesspiece == '_')
+// setPieceLabel
+// Purpose:     Set the pieceLabel as the argument
+// Arguments:   piece (char)
+// Returns:     None
+void Chesspiece::setPieceLabel(char piece){
+    if (piece == 'X' || piece == 'O' || piece == '_')
     {
-        pieceLabel = Chesspiece;
+        pieceLabel = piece;
     } else {
         pieceLabel = ' ';
     }
--- Chesspiece.h
+++ Chesspiece.h
@@ -3,10 +3,10 @@
 
 #ifndef CHESSPIECE_H
 #define CHESSPIECE_H
+
 class Chesspiece {
 public:
     Chesspiece();
-    Chesspiece(char piece);
     const char getPieceLabel();
     void setPieceLabel(char Chesspiece);
 private:
